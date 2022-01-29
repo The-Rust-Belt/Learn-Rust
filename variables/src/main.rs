c321dfa849d4e5cf6bb430ae088dc5fa4cfef6ea
jpcaulfi <joecaulfield29@yahoo.com>
2022-01-28 22:47:26 -0500

    Fixed variable print-outs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,30 +4,30 @@ fn main() {
     println!("The value of Pi is {}", PI);
 
     let a; // Declaration
-    println!(a);
+    println!("{}", a);
 
     a = 5;
-    println!(a);
+    println!("{}", a);
 
     let b: i8; // Declaration with data type
-    println!(b);
+    println!("{}", b);
 
     b = 5;
-    println!(b);
+    println!("{}", b);
 
     let t = true; // Data type is implicit
-    println!(t);
+    println!("{}", t);
 
     let f: bool = false;
-    println!(f);
+    println!("{}", f);
 
     let(x, y) = (1, 2); // x = 1 and y = 2
-    println!(x, y);
+    println!("{}, {}", x, y);
 
     let mut z = 5; // Mutable variable
-    println!(z);
+    println!("{}", z);
     z = 6;
-    println!(z);
+    println!("{}", z);
 
     let z = {
         let x = 1;
@@ -35,7 +35,7 @@ fn main() {
 
         x + y
     }; // z = 3
-    println!(z);
+    println!("{}", z);
 
 
     const M: i32 = 5;
